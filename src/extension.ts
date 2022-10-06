1230d4bca19a5180870f87b64895f4cf7909b8b9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-10-05 21:56:40 -0400

    fix: handle the minutiae of auto-indent

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,8 @@
+import { Mutex } from 'async-mutex'
 import * as vscode from 'vscode'
 import { jumpBackward, jumpForward } from './traversal'
+
+const lock = new Mutex()
 
 export function activate(context: vscode.ExtensionContext) {
   const move = (getPosition: typeof jumpBackward | typeof jumpForward) => {
@@ -21,8 +24,12 @@ export function activate(context: vscode.ExtensionContext) {
 
   let lastKnownTextEditor = null as vscode.TextEditor | null
   let lastKnownLineNumber = -1
+  let lastKnownLineText = ''
+
+  const output = vscode.window.createOutputChannel('Tab Traversal')
 
   context.subscriptions.push(
+    output,
     vscode.commands.registerCommand('jumpForward', () => {
       const selections = getSelections()
       if (selections.length > 1) {
@@ -39,33 +46,150 @@ export function activate(context: vscode.ExtensionContext) {
         move(jumpBackward)
       }
     }),
-    vscode.window.onDidChangeTextEditorSelection(event => {
-      const { selections, textEditor } = event
-      const lineNumber = selections[0].start.line
-      if (
-        lastKnownLineNumber === lineNumber &&
-        lastKnownTextEditor === textEditor
-      ) {
-        return
-      }
-      lastKnownLineNumber = lineNumber
-      lastKnownTextEditor = textEditor
-      if (
-        selections.length == 1 &&
-        selections[0].isEmpty &&
-        textEditor === vscode.window.activeTextEditor
-      ) {
-        const line = textEditor.document.lineAt(lineNumber)
-        if (line.firstNonWhitespaceCharacterIndex == line.text.length) {
-          vscode.commands.executeCommand('editor.action.reindentselectedlines')
+    vscode.window.onDidChangeTextEditorSelection(async event => {
+      const unlock = await lock.acquire()
+      try {
+        const { selections, textEditor } = event
+        const lineNumber = selections[0].active.line
+
+        if (textEditor === vscode.window.activeTextEditor)
+          output.appendLine(
+            `onDidChangeTextEditorSelection: ${JSON.stringify({
+              lastKnownTextEditor: lastKnownTextEditor?.document.fileName,
+              lastKnownLineNumber,
+              textEditor: textEditor.document.fileName,
+              lineNumber,
+            })}`
+          )
+
+        if (
+          textEditor === vscode.window.activeTextEditor &&
+          (lastKnownTextEditor !== textEditor ||
+            lastKnownLineNumber !== lineNumber)
+        ) {
+          const revertedLine =
+            lastKnownTextEditor &&
+            replaceLineIfOnlyWhitespace(
+              lastKnownTextEditor,
+              lastKnownLineNumber,
+              lastKnownLineText
+            )
+
+          if (revertedLine) {
+            output.appendLine(`Reverting line ${lastKnownLineNumber}`)
+          }
+
+          const line = await reindentLineIfAllowed(
+            textEditor,
+            lineNumber,
+            output
+          )
+
+          lastKnownLineNumber = line ? lineNumber : -1
+          lastKnownTextEditor = line ? textEditor : null
+          lastKnownLineText = line ? line.text : ''
         }
+      } finally {
+        unlock()
       }
     })
   )
 }
 
-export function deactivate() {}
+export function deactivate() {
+  lock.cancel()
+}
+
+async function reindentLineIfAllowed(
+  textEditor: vscode.TextEditor,
+  lineNumber: number,
+  output: vscode.OutputChannel
+) {
+  const { selections } = textEditor
+  if (
+    lineNumber > 0 &&
+    selections.length === 1 &&
+    selections[0].isEmpty &&
+    !textEditor.document.fileName.includes('/node_modules/')
+  ) {
+    const line = textEditor.document.lineAt(lineNumber)
+    if (line.isEmptyOrWhitespace) {
+      const oldText = line.text
+
+      // If the line before this one is empty, we have to detect the
+      // indentation manually, because of this bug:
+      // https://github.com/microsoft/vscode/issues/121214
+      const hasEmptyLineBefore = textEditor.document.lineAt(
+        lineNumber - 1
+      ).isEmptyOrWhitespace
+
+      if (hasEmptyLineBefore) {
+        // Find the first non-empty line before this one.
+        let i = lineNumber - 1
+        while (--i >= 0) {
+          const lineBefore = textEditor.document.lineAt(i)
+          if (!lineBefore.isEmptyOrWhitespace) {
+            break
+          }
+          if (i === 0) {
+            return
+          }
+        }
+
+        // Update the editor selection to include this non-empty line and the reindented line.
+        textEditor.selection = new vscode.Selection(
+          i + 1,
+          0,
+          lineNumber,
+          line.text.length
+        )
+      }
+
+      await vscode.commands.executeCommand(
+        'editor.action.reindentselectedlines'
+      )
+      if (hasEmptyLineBefore) {
+        // Restore the original selection.
+        textEditor.selection = selections[0]
+      }
+      // Extract the result of "Reindent selected lines" action.
+      const newText = textEditor.document.lineAt(lineNumber).text
+      if (oldText !== newText) {
+        output.appendLine(`Reindenting line ${lineNumber}`)
+        output.appendLine(`${JSON.stringify({ newText, oldText })}`)
+        await vscode.commands.executeCommand('undo')
+        replaceLineIfOnlyWhitespace(textEditor, lineNumber, newText)
+      }
+
+      return line
+    }
+  }
+}
 
 function getSelections() {
   return vscode.window.activeTextEditor?.selections || []
+}
+
+function replaceLineIfOnlyWhitespace(
+  textEditor: vscode.TextEditor,
+  lineNumber: number,
+  newText: string
+) {
+  const line = textEditor.document.lineAt(lineNumber)
+  if (line.isEmptyOrWhitespace) {
+    textEditor.edit(
+      editor =>
+        line.text.length > 0
+          ? editor.replace(
+              new vscode.Range(lineNumber, 0, lineNumber, line.text.length),
+              newText
+            )
+          : editor.insert(new vscode.Position(lineNumber, 0), newText),
+      {
+        undoStopBefore: false,
+        undoStopAfter: false,
+      }
+    )
+    return line
+  }
 }
